fa71e2f75e0b1ae7dae8c950dc431b3953519adf
Tiffany Monroe <hello@tiffanymonroe.com>
2017-11-12 16:45:41 -0800

    contents from w03d05 homework

---
+++ app.js
@@ -0,0 +1,80 @@
+// console.log("hello, it's me");
+$(()=> {
+
+//Global scope
+const words = ["line", "stanza", "meter", "rhyme", "sound"];
+
+const chooseWord = () => {
+  const randomIndex = Math.floor(Math.random() * (6 - 0)) + 0;
+  return words[randomIndex];
+}
+
+let guesses = 0;
+let guessedLetters = [];
+
+
+//Local scope
+
+
+class Letter = {
+  constructor(value, hidden){
+    this.value = value;
+    this.hidden = true;
+  },
+  show(){
+    return this.hidden = false;
+  },
+  display(){
+    if (this.hidden === true){
+      chooseWord.charAt().replaceWith('_');
+      }
+    else Letter.show();
+    console.log(chooseWord.charAt().val());
+  }
+}
+
+c Word = {
+  letters: [],
+  isFound(){},
+  test(letter){
+    if (letter === chooseWord().charAt().val()){
+      show();
+    }
+    else {
+      console.log("this letter doesn't match any in the word");
+    }
+  },
+  render(){}
+
+}
+
+
+
+const startGame = () => {
+  guesses = 10,
+  chooseWord();
+}
+
+startGame();
+
+$('#word').append(chooseWord());
+
+
+
+//Event listeners
+
+$('input').on('keypress', ()=> {
+  console.log('the key had been pressed');
+  $('button').on('click', ()=>{
+  let letter = $('input').val();
+  guessedLetters.push(letter);
+  console.log(guessedLetters);
+  })
+});
+//looks for the user to enter a letter (keypress), pushes value of the letter to the guessedLetters array.
+
+
+
+
+
+}); //end window.onload
