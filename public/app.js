978ddf82c40c8d94bed4635e44a7042c94731472
Tiffany Monroe <hello@tiffanymonroe.com>
2017-11-12 21:25:49 -0800

    get word and alphabet buttons function

--- public/app.js
+++ public/app.js
@@ -2,32 +2,35 @@ const app = angular.module('HangmanGame', []);
 
 app.controller('GameController', ['$http', function($http){
   const controller = this;
-  
+
   this.words = [];
-  this.currentWord = '';
   this.letter = '';
+  this.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','t','u','v','w','x','y','z'];
 
   this.game = {
-    controller.guesses = 10;
-    controller.guessedLetters = [];
-    controller.isOver = function(){
+    guesses: 10,
+    guessedLetters: [],
+    isOver(){
       //
     },
-    controller.overMessage = function(){
+    overMessage(){
     }
   };
 
 
+  this.chooseLetter = function(){
+
+  }
 
 
-
-  this.getWord = function() {
+  this.getWord = function(){
     $http({
             method: "GET",
             url: 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&excludePartOfSpeech=abbreviation&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=2&maxLength=7&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
           }).then(
             function(res){
               controller.word = res.data.word;
+              controller.words.push(controller.word);
             },
             function(err){
               console.log('getWord error is: ', err);
